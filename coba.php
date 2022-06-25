674f7c4f188117d53e52102e721dea182b4a3603
Ahmad sibly <ziblyvila12@gmail.com>
2022-06-25 10:14:21 +0800

    membuat class Produk

---
+++ coba.php
@@ -0,0 +1,12 @@
+<?php
+
+class Coba
+{
+}
+
+// membut 3 buah object dari class Coba
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
---
+++ produk.php
@@ -0,0 +1,12 @@
+<?php 
+
+// Studi kasus produk
+// menjual komik dan game
+
+class Produk
+{
+    
+}
+
+$poduk1 = new Produk();
+$produk2 = new Produk();
